93ef9eef932f14a3ad4d7aa4471a9675618f33bd
ldittmar <ldittmar.privat@gmail.com>
2017-02-22 22:30:54 +0100

    first commit
    first commit

---
+++ main.js
@@ -0,0 +1,89 @@
+/**
+ *
+ *      ioBroker Fronius inverters Adapter
+ *
+ *      (c) 2017 ldittmar <iobroker@lmdsoft.de>
+ *
+ *      MIT License
+ *
+ */
+
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+"use strict";
+
+// you have to require the utils module and call adapter function
+var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+var request = require('request');
+var ping = require("ping");
+
+// you have to call the adapter function and pass a options object
+// name has to be set and has to be equal to adapters folder name and main file name excluding extension
+// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
+var adapter = utils.adapter('fronius');
+
+var ip;
+
+// is called when adapter shuts down - callback has to be called under any circumstances!
+adapter.on('unload', function (callback) {
+    try {
+        adapter.log.info('cleaned everything up...');
+        callback();
+    } catch (e) {
+        callback();
+    }
+});
+
+// is called if a subscribed object changes
+adapter.on('objectChange', function (id, obj) {
+    // Warning, obj can be null if it was deleted
+    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+});
+
+// is called if a subscribed state changes
+adapter.on('stateChange', function (id, state) {
+    // Warning, state can be null if it was deleted
+    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
+
+    // you can use the ack flag to detect if it is status (true) or command (false)
+    if (state && !state.ack) {
+        adapter.log.info('ack is not set!');
+    }
+});
+
+// Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
+adapter.on('message', function (obj) {
+    if (typeof obj == 'object' && obj.message) {
+        if (obj.command == 'send') {
+            // e.g. send email or pushover or whatever
+            console.log('send command');
+
+            // Send response in callback if required
+            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+        }
+    }
+});
+
+// is called when databases are connected and adapter received configuration.
+// start here!
+adapter.on('ready', function () {
+    main();
+});
+
+function main() {
+
+	// The adapters config (in the instance object everything under the attribute "native") is accessible via
+    // adapter.config:
+
+    ip = adapter.config.ip;
+	
+	 if (ip) {
+
+       
+
+    } else {
+        adapter.log.error("Please configure the Fronius Adapter");
+    }
+
+
+}
