5622216a89b1293680f26a36f29083cebb1b8894
Ingo Fischer <github@fischer-ka.de>
2018-01-02 13:34:07 +0100

    fix util.js usage and use utils.Adapter

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ var ping = require(__dirname + '/lib/ping');
 // you have to call the adapter function and pass a options object
 // name has to be set and has to be equal to adapters folder name and main file name excluding extension
 // adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
-var adapter = utils.adapter('fronius');
+var adapter = utils.Adapter('fronius');
 
 var ip, baseurl, apiver;
 var hybrid = false;
