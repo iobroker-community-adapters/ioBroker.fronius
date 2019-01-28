7b582277414256011520e2c9d8ed884ea055788c
ldittmar <ldittmar.privat@gmail.com>
2019-01-28 21:53:03 +0100

    Add yandex translation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /*!
  * ioBroker gulpfile
- * Date: 2019-01-22
+ * Date: 2019-01-28
  */
 "use strict";
 
@@ -58,7 +58,7 @@ function readWordJs(src) {
         }
         words = words.substring(words.indexOf('{'), words.length);
         words = words.substring(0, words.lastIndexOf(';'));
-     
+
         const resultFunc = new Function("return " + words + ";");
 
         return resultFunc();
@@ -334,7 +334,7 @@ function languages2words(src) {
     writeWordJs(bigOne, src);
 }
 
-async function translateNotExisting(obj, baseText) {
+async function translateNotExisting(obj, baseText, yandex) {
     let t = obj['en'];
     if (!t) {
         t = baseText;
@@ -342,8 +342,10 @@ async function translateNotExisting(obj, baseText) {
 
     if (t) {
         for (let l in languages) {
-            if (!obj[l]) {
-                obj[l] = await translate(t, l);
+            if (!obj[l]) {                
+                const time = new Date().getTime();
+                obj[l] = await translate(t, l, yandex);
+                console.log("en -> " + l + " " + (new Date().getTime() - time) + " ms");
             }
         }
     }
@@ -421,30 +423,42 @@ gulp.task('updateReadme', function (done) {
 });
 
 gulp.task('translate', async function (done) {
+
+    let yandex;
+    const i = process.argv.indexOf("--yandex");
+    if (i > -1) {
+        yandex = process.argv[i + 1];
+    }
+    
     if (iopackage && iopackage.common) {
         if (iopackage.common.news) {
+            console.log("Translate News");
             for (let k in iopackage.common.news) {
+                console.log("News: " + k);
                 let nw = iopackage.common.news[k];
-                await translateNotExisting(nw);
+                await translateNotExisting(nw, null, yandex);
             }
         }
         if (iopackage.common.titleLang) {
-            await translateNotExisting(iopackage.common.titleLang, iopackage.common.title);
+            console.log("Translate Title");
+            await translateNotExisting(iopackage.common.titleLang, iopackage.common.title, yandex);
         }
         if (iopackage.common.desc) {
-            await translateNotExisting(iopackage.common.desc);
+            console.log("Translate Description");
+            await translateNotExisting(iopackage.common.desc, null, yandex);
         }
 
         if (fs.existsSync('./admin/i18n/en/translations.json')) {
             let enTranslations = require('./admin/i18n/en/translations.json');
             for (let l in languages) {
+                console.log("Translate Text: " + l);
                 let existing = {};
                 if (fs.existsSync('./admin/i18n/' + l + '/translations.json')) {
                     existing = require('./admin/i18n/' + l + '/translations.json');
                 }
                 for (let t in enTranslations) {
                     if (!existing[t]) {
-                        existing[t] = await translate(enTranslations[t], l);
+                        existing[t] = await translate(enTranslations[t], l, yandex);
                     }
                 }
                 if (!fs.existsSync('./admin/i18n/' + l + '/')) {
